09c97182660ad8a81e41b8b762dc1485c5459838
ImmoVesta <ricky.ferrara.95@gmail.com>
2025-07-07 16:28:09 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 function showMessage() {
 document.getElementById("message").textContent = "Willkommen bei ImmoVesta!";
 }
+document.querySelector("form").addEventListener("submit", function (e) {
+  e.preventDefault(); // Previene l'invio reale
+  alert("✅ Nachricht erfolgreich gesendet! Wir melden uns bald bei Ihnen.");
+  this.reset(); // Resetta i campi del modulo
+});
