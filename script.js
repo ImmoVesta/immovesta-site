e6d60807d0975dc8507f8b1c1a977166cb0a06d0
ImmoVesta <ricky.ferrara.95@gmail.com>
2025-07-08 20:43:40 +0200

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,13 @@ document.querySelector("form").addEventListener("submit", function (e) {
   alert("✅ Nachricht erfolgreich gesendet! Wir melden uns bald bei Ihnen.");
   this.reset(); // Resetta i campi del modulo
 });
+<script>
+  function toggleSection(id) {
+    const panels = document.querySelectorAll(".legal-panel");
+    panels.forEach(panel => {
+      panel.style.display = "none";
+    });
+    const selected = document.getElementById(id);
+    if (selected) selected.style.display = "block";
+  }
+</script>
